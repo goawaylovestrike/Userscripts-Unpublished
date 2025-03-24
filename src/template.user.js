0f5d2e2c106e0a5d2dbf5024e28ca333eebafa0b
GoAway <goawaylovestrike@gmail.com>
2025-03-24 16:17:16 -0700

    added template

---
+++ src/template.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Script Name
+// @namespace    https://github.com/GoAwayLoveStrike
+// @version      0.1
+// @description  Script description
+// @author       GoAwayLoveStrike
+// @match        https://example.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here
+})();
