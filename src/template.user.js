7778c458faeb336c003cd7c30a9305f1842d5ceb
GoAway <goawaylovestrike@gmail.com>
2025-03-25 08:30:29 -0700

    updated template

--- src/template.user.js
+++ src/template.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Script Name
-// @namespace    https://github.com/GoAwayLoveStrike
+// @namespace    https://github.com/goawaylovestrike/Userscripts/script-name/
 // @version      0.1
 // @description  Script description
 // @author       GoAwayLoveStrike
 // @match        https://example.com/*
+// @updateURL    https://github.com/goawaylovestrike/Userscripts/raw/refs/heads/main/Scripts/script-name/script-name.js
+// @downloadURL  https://github.com/goawaylovestrike/Userscripts/raw/refs/heads/main/Scripts/script-name/script-name.js
 // @grant        none
 // ==/UserScript==
 
